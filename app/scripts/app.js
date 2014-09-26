f90f8b1e7737ed9ed93336645f56a4a488dad1c3
Shreyance <shreyance@intelligrape.com>
2014-09-26 21:07:37 +0530

    created sample app shown on site

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,16 @@
-(function(document) {
-  'use strict';
+(function (document) {
+    'use strict';
 
-  document.addEventListener('polymer-ready', function() {
-    // Perform some behaviour
-    console.log('Polymer is ready to rock!');
-  });
+    var tabs = document.querySelector('paper-tabs');
+    var list = document.querySelector('post-list');
+
+    tabs.addEventListener('core-select', function() {
+        list.show = tabs.selected;
+    });
+    document.addEventListener('polymer-ready', function () {
+        // Perform some behaviour
+        console.log('Polymer is ready to rock!');
+    });
 
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
